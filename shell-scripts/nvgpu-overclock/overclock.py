e355f0d7fcc042fb5a04c69185b60e0726f25d7d
Beau Taapken <beau@lioncode.nl>
2025-01-19 23:00:02 +0100

    automatic push

--- shell-scripts/nvgpu-overclock/overclock.py
+++ shell-scripts/nvgpu-overclock/overclock.py
@@ -5,11 +5,11 @@ nvmlInit()
 myGPU = nvmlDeviceGetHandleByIndex(0)
 
 # The GPU frequency offset value should replace the "80" in the line below.
-nvmlDeviceSetGpcClkVfOffset(myGPU, 45)
+nvmlDeviceSetGpcClkVfOffset(myGPU, 60)
 
 # The Mem frequency Offset should be **multiplied by 2** to replace the "2500" below
 # for example, an offset of 500 in GWE means inserting a value of 1000 in the next line
-nvmlDeviceSetMemClkVfOffset(myGPU, 3000)
+nvmlDeviceSetMemClkVfOffset(myGPU, 2800)
 
 # NOTE: I don't need this, already done with a separate script
 # The power limit should be set below in mW - 216W becomes 216000, etc. May not work for laptops. Remove the below line if you don't want to adjust power limits.
