08413d9b4c4bbb6adec4b6f045fa704f5be0013a
Beau Taapken <beau@lioncode.nl>
2025-11-28 22:00:02 +0100

    automatic push

--- .config/jellyfin-mpv-shim/scripts/main.lua
+++
@@ -1,2 +0,0 @@
-dofile("/home/beau/.config/mpv/scripts/chapterseek.lua")
-dofile("/home/beau/.config/mpv/scripts/persist-properties.lua")
--- shell-scripts/old/standby.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-START=`date +%s`
-while [ $(( $(date +%s) - 40 )) -lt $START ]; do
-    xset dpms force standby
-done
