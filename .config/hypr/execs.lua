f3d534fe7ed838040814550b4c8b36d593217348
Beau Taapken <beau@lioncode.nl>
2026-09-25 22:00:02 +0200

    automatic push

--- .config/hypr/execs.lua
+++ .config/hypr/execs.lua
@@ -5,7 +5,7 @@ hl.on("hyprland.start", function()
 	hl.exec_cmd("wl-paste --type image --watch cliphist store")
 	hl.exec_cmd("systemctl --user start hyprpolkitagent")
 	hl.exec_cmd("fcitx5")
-	hl.exec_cmd("firefox & steam & thunderbird & slack --ozone-platform=wayland & signal-desktop")
+	hl.exec_cmd("firefox & steam & thunderbird & slack --ozone-platform=wayland & signal-desktop & linux-soundboard")
 	hl.exec_cmd(
 		"sleep 10; vesktop --ozone-platform=wayland & jellium-desktop & lact & /mnt/crucial-ssd/SAN/san.AppImage"
 	)
